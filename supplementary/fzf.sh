17247209572547e3fabc82f79f43bde6323214a9
Jake Price <git@jakeprice.dev>
2022-03-30 16:35:57 +0100

    Support DISTRO_IS variable

--- supplementary/fzf.sh
+++ supplementary/fzf.sh
@@ -3,16 +3,21 @@
 # :::: fzf Stuff ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 # fzf fuzzy completion:
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "$DISTRO_IS" == "fedora" ]]; then
 	[ -f /usr/share/fzf/shell/key-bindings.bash ] && source /usr/share/fzf/shell/key-bindings.bash
+    export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
+elif [[ "$DISTRO_IS" == "debian" ]]; then
+	[ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+    export FZF_DEFAULT_COMMAND='fdfind --hidden --follow --exclude ".git"'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	[ -f $HOME/.fzf.bash ] && source $HOME/.fzf.bash
+    export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
 elif [[ "$OSTYPE" == "linux-android" ]]; then
 	source $PREFIX/share/fzf/completion.bash
+    export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
 fi
 
 # Override default fzf commands:
-export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 export FZF_ALT_C_COMMAND=$FZF_DEFAULT_COMMAND
 
