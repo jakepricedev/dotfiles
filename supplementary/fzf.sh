6f433226322b6082d5bcc661b07e89092f7b236d
Jake Price <git@jakeprice.dev>
2022-08-17 15:36:00 +0100

    Fix completion/bindings and fd exclude for MacOS

--- supplementary/fzf.sh
+++ supplementary/fzf.sh
@@ -6,12 +6,35 @@
 if [[ "$DISTRO_IS" == "fedora" ]]; then
 	[ -f /usr/share/fzf/shell/key-bindings.bash ] && source /usr/share/fzf/shell/key-bindings.bash
     export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
+
 elif [[ "$DISTRO_IS" == "debian" ]]; then
 	[ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
     export FZF_DEFAULT_COMMAND='fdfind --hidden --follow --exclude ".git"'
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	[ -f $HOME/.fzf.bash ] && source $HOME/.fzf.bash
-    export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
+
+    # Completion & Bindings:
+    PATH_COMPLETION=$HOMEBREW_PREFIX/opt/fzf/shell/completion.bash
+    PATH_BINDINGS=$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.bash
+	[ -f $PATH_COMPLETION ] && source $PATH_COMPLETION
+	[ -f $PATH_BINDINGS ] && source $PATH_BINDINGS
+
+    # Default fzf command:
+    export FZF_DEFAULT_COMMAND='fd \
+        --hidden \
+        --follow \
+        --exclude "*.DS_Store" \
+        --exclude "*.ansible" \
+        --exclude "*.azure" \
+        --exclude "*.cache" \
+        --exclude "*.git" \
+        --exclude "*antenv" \
+        --exclude "*Google Drive" \
+        --exclude "*Library" \
+        --exclude "*lib" \
+        --exclude "*venv" \
+        --exclude "*.pyc"'
+
 elif [[ "$OSTYPE" == "linux-android" ]]; then
 	source $PREFIX/share/fzf/completion.bash
     export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git"'
