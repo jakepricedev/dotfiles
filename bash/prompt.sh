cc00bd608ad62667e944874949ce23bd8a9f4914
Jake Price <git@jakeprice.dev>
2023-08-18 11:42:14 +0100

    Updates to server config

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-# Get current git branch when applicable:
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
-
 # Set prompt:
 SERVER_COLOUR="\e[1m\e[38;5;9m"
-FEDORA_COLOUR="\e[1m\e[38;5;75m"
-UBUNTU_COLOUR="\e[1m\e[38;5;90m"
-DEBIAN_COLOUR="\e[1m\e[38;5;198m"
-MACOS_COLOUR="\e[1m\e[38;5;208m"
-TERMUX_COLOUR="\e[1m\e[92m"
 
-if [[ "$IS_A_SERVER" == 1 ]]; then
-    export PS1="\[$SERVER_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-elif [[ "$DISTRO_IS" == "fedora" ]]; then
-    export PS1="\[$FEDORA_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-elif [[ "$DISTRO" == "Ubuntu" ]]; then
-    export PS1="\[$UBUNTU_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-elif [[ "$DISTRO_IS" == "debian" ]]; then
-    export PS1="\[$DEBIAN_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export PS1="\[$MACOS_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-elif [[ "$OSTYPE" == "linux-android" ]]; then
-    export PS1="\[$TERMUX_COLOUR\]\u@\h [ \t ] \w\e[0m\$(parse_git_branch)\n$ "
-fi
-
+export PS1="\[$SERVER_COLOUR\]\u@\h [ \t ] \w\e[0m\n$ "
--- bash/session_intro.sh
+++ bash/session_intro.sh
@@ -2,20 +2,10 @@
 
 # Custom info to show on session launch:
 HOSTNAME=$(hostname)
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    DISTRO=$(grep --no-filename "^NAME" /etc/*-release \
-    | sed "s/NAME=//; s/\"//g")
-    DISTRO_VERSION=$(grep --no-filename "VERSION_ID" /etc/*-release \
-        | sed "s/^VERSION_ID=//; s/\"//g")
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    DISTRO=$(sw_vers -productName)
-    DISTRO_VERSION=$(sw_vers -productVersion)
-elif [[ "$OSTYPE" == "linux-android" ]]; then
-    DISTRO=$(echo "Termux")
-    DISTRO_VERSION=$TERMUX_VERSION
-fi
-
+DISTRO=$(grep --no-filename "^NAME" /etc/*-release | sed "s/NAME=//; s/\"//g")
+DISTRO_VERSION=$(grep --no-filename "VERSION_ID" /etc/*-release | sed "s/^VERSION_ID=//; s/\"//g")
 IP_PUBLIC=$(dig @ns1-1.akamaitech.net ANY whoami.akamai.net +short)
+
 echo -e "Host:          $HOSTNAME
 OS:            $DISTRO $DISTRO_VERSION
 Public IP:     $IP_PUBLIC
